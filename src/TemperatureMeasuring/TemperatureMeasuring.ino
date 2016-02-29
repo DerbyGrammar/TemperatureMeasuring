7c5acd889d324c455c25659fd6fc6ff40eb2f150
ThorinDev <c.nethercott@icloud.com>
2016-02-29 13:20:08 +0000

    Using blackProbe instead of black_probe

--- src/TemperatureMeasuring/TemperatureMeasuring.ino
+++ src/TemperatureMeasuring/TemperatureMeasuring.ino
@@ -16,16 +16,16 @@ OneWire oneWire(ONE_WIRE_BUS_PIN);
 DallasTemperature sensors(&oneWire);
 
 //Sets the addresses for each probe on the one wire bus
-DeviceAddress black_probe = { 0x28, 0xFF, 0x1A, 0xAA, 0x62, 0x15, 0x03, 0x20 }; //Black Address
-DeviceAddress red_probe   = { 0x28, 0xFF, 0xA9, 0xB4, 0x62, 0x15, 0x03, 0x0C }; //Red Address
-DeviceAddress green_probe = { 0x28, 0xFF, 0xF5, 0xB3, 0x62, 0x15, 0x03, 0x3E }; //Green Address
+DeviceAddress blackProbe = { 0x28, 0xFF, 0x1A, 0xAA, 0x62, 0x15, 0x03, 0x20 }; //Black Address
+DeviceAddress redProbe   = { 0x28, 0xFF, 0xA9, 0xB4, 0x62, 0x15, 0x03, 0x0C }; //Red Address
+DeviceAddress greenProbe = { 0x28, 0xFF, 0xF5, 0xB3, 0x62, 0x15, 0x03, 0x3E }; //Green Address
 
 void setup()
 {
   sensors.begin();
-  sensors.setResolution(black_probe, 10);
-  sensors.setResolution(red_probe, 10);
-  sensors.setResolution(green_probe, 10);
+  sensors.setResolution(blackProbe, 10);
+  sensors.setResolution(redProbe, 10);
+  sensors.setResolution(greenProbe, 10);
 }
 
 void loop()
@@ -38,21 +38,21 @@ void loop()
   lcd.print("DGS Racing");
   lcd.setCursor(0, 1);
   lcd.print("Black = ");
-  printTemperature(black_probe);
+  printTemperature(blackProbe);
 
   delay(2000);
   lcd.clear();
   lcd.print("DGS Racing");
   lcd.setCursor(0, 1);
   lcd.print("Red   = ");
-  printTemperature(red_probe);
+  printTemperature(redProbe);
 
   delay(2000);
   lcd.clear();
   lcd.print("DGS Racing");
   lcd.setCursor(0, 1);
   lcd.print("Green = ");
-  printTemperature(green_probe);
+  printTemperature(greenProbe);
 }
 
 void printTemperature(DeviceAddress deviceAddress)
