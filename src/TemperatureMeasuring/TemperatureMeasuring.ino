047434c3fa8fb2099d015252beb33c0ebb76b006
Thorin <c.nethercott@icloud.com>
2016-03-08 10:36:37 +0000

    Update TemperatureMeasuring.ino

--- src/TemperatureMeasuring/TemperatureMeasuring.ino
+++ src/TemperatureMeasuring/TemperatureMeasuring.ino
@@ -8,19 +8,19 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-//Pin Numbers
+/* Pin Numbers */
 #define ONE_WIRE_BUS_PIN 6
 
-//Library Stuff
+/* Library Setup */
 OneWire oneWire(ONE_WIRE_BUS_PIN);
 DallasTemperature sensors(&oneWire);
 
-//Sets the addresses for each probe on the one wire bus
+/* Sets the addresses for each probe on the one wire bus */
 DeviceAddress blackProbe = { 0x28, 0xFF, 0x1A, 0xAA, 0x62, 0x15, 0x03, 0x20 }; //Black Address
 DeviceAddress redProbe   = { 0x28, 0xFF, 0xA9, 0xB4, 0x62, 0x15, 0x03, 0x0C }; //Red Address
 DeviceAddress greenProbe = { 0x28, 0xFF, 0xF5, 0xB3, 0x62, 0x15, 0x03, 0x3E }; //Green Address
 
-// Variables
+/* Variables */
 int delayTime;
 
 void setup() {
